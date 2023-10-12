a69019811d238db737d5e07289431ddcd345f2b7
AYBERK TOMBUL <ayberktombul@gmail.com>
2023-10-12 16:56:54 +0300

    Add project files.

---
+++ FineBlog/Models/ApplicationUser.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace FineBlog.Models
+{
+    public class ApplicationUser:IdentityUser
+    {
+        public string? Firstname { get; set; }
+        public string? Lastname { get; set;}
+        public List<Post>? Posts { get; set; }
+    }
+}
---
+++ FineBlog/Models/Post.cs
@@ -0,0 +1,15 @@
+﻿namespace FineBlog.Models
+{
+    public class Post
+    {
+        public int Id { get; set; }
+        public string? title { get; set; }
+        public string? ShortDescription { get; set; }
+        //relation
+        public string? ApplicationUserId { get; set; }
+        public ApplicationUser? ApplicationUser { get; set; }
+        public DateTime CreatedDate { get; set; } = DateTime.Now;
+        public string? Description { get; set; }
+        public string? Slug { get; set; }
+    }
+}
