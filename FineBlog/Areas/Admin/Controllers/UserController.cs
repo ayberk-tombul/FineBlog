77b15275e4c02b5da77607695e37b59dce30b2b5
AYBERK TOMBUL <ayberktombul@gmail.com>
2023-10-14 14:38:15 +0300

    Register admin succeeded.

--- FineBlog/Areas/Admin/Controllers/UserController.cs
+++ FineBlog/Areas/Admin/Controllers/UserController.cs
@@ -1,5 +1,6 @@
 ﻿using AspNetCoreHero.ToastNotification.Abstractions;
 using FineBlog.Models;
+using FineBlog.Utilities;
 using FineBlog.ViewModels;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
@@ -34,6 +35,57 @@ namespace FineBlog.Areas.Admin.Controllers
                 LastName = x.Lastname,
                 UserName = x.UserName,
             }).ToList();
+            return View(vm);
+        }
+        [Authorize(Roles = "Admin")]
+        [HttpGet]
+        public IActionResult Register()
+        {
+            return View(new RegisterVM());
+        }
+
+        [Authorize(Roles = "Admin")]
+        [HttpPost]
+        public async Task<IActionResult> Register(RegisterVM vm)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(vm);
+            }
+            var checkUserByEmail= await _userManager.FindByEmailAsync(vm.Email);
+            if (checkUserByEmail != null)
+            {
+                _notification.Error("Bu E-Postayla zaten bir kayıt mevcut");
+                return View(vm);
+            }
+            var checkUserByUsename = await _userManager.FindByNameAsync(vm.UserName);
+            if(checkUserByUsename != null)
+            {
+                _notification.Error("Bu Kullanıcı Adı Mevcut");
+                return View(vm);
+            }
+
+            var applicationUser = new ApplicationUser()
+            {
+                Email = vm.Email,
+                UserName = vm.UserName,
+                Firstname = vm.FirstName,
+                Lastname = vm.LastName,
+            };
+            var result = await _userManager.CreateAsync(applicationUser,vm.Password);
+            if (result.Succeeded)
+            {
+                if (vm.IsAdmin)
+                {
+                    await _userManager.AddToRoleAsync(applicationUser, WebsiteRoles.WebsiteAdmin);
+                }
+                else
+                {
+                    await _userManager.AddToRoleAsync(applicationUser,WebsiteRoles.WebsiteAuthor);
+                }
+                _notification.Success("Kayıt Başarılı.");
+                return RedirectToAction("Index","User", new {area="Admin"});
+            }
             return View(vm);
         }
 
